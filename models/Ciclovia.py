35586a68a6df7e1dcb7e2b3bcdc1dcec1a1ffda0
Jhoan Gutierrez <jhoangutierrezc@gmail.com>
2023-09-28 23:11:59 -0500

    tabla ciclovia

---
+++ models/Ciclovia.py
@@ -0,0 +1,22 @@
+from config.db import app, db, ma
+
+class Ciclovia(db.Model):
+    __tablename__ = "ciclovia"
+
+    id = db.Column(db.Integer, primary_key = True)
+    punto_inicial = db.Column(db.String(50))
+    punto_final = db.Column(db.String(50))
+    descripcion = db.Column(db.String(50))
+    nombre_ciclovia = db.Column(db.String(50))
+
+    def __init__(self, pinicial, pfinal, desc, nciclo):
+        self.punto_inicial = pinicial
+        self.punto_final = pfinal
+        self.descripcion = desc
+        self.nombre_ciclovia = nciclo
+with app.app_context():
+    db.create_all()
+
+class UsuarioSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'punto_inicial','punto_final','descripcion','nombre_ciclovia')  
