99e8444ad538b5cf99871d1eef0b2552d8bd86ae
Diegdary <diegojose3019@gmail.com>
2023-09-28 23:41:39 -0500

    'ruta'

--- models/Ruta.py
+++ models/Ruta.py
@@ -1,6 +1,6 @@
 from config.db import app, db, ma
 
-class Usuario(db.Model):
+class Ruta(db.Model):
     __tablename__ = "tblruta"
 
     id = db.Column(db.Integer, primary_key = True)
@@ -10,15 +10,18 @@ class Usuario(db.Model):
     nombre_ruta = db.Column(db.String(50))
     ciclovia_inicial = db.Column(db.String(50))
     ciclovia_final = db.Column(db.String(50))
-    
 
     def __init__(self,a,b,c,d,f,g):
-        self.id = a
-        self.id_usuario = id_usu
-        self.mensaje = mens
+        self.id_alerta = a
+        self.id_usuario = b
+        self.descripcion_ruta = c
+        self.nombre_ruta = d
+        self.ciclovia_inicial = f
+        self.ciclovia_final = g
+
 with app.app_context():
     db.create_all()
 
 class RutaSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'id_usuario','mensaje')
+        fields = ('id', 'id_alerta','id_usuario','descripcion_ruta', 'nombre_ruta', 'ciclovia_inicial', 'ciclovia_final')
